ca98a2d5e10275d6a5aea545cb0a4e51a7ca5252
Ramindur Singh <ramindurs@gmail.com>
2023-12-20 08:44:20 +0000

    RELEASE - 0.7.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,21 @@ import sbt._
 
 object Dependencies {
 
+  private lazy val simexVersion = "0.7.4"
+
   lazy val all = Seq(
-    "thediscprog" %% "util-library" % "0.7.0",
+    "simex" %% "simex-messaging" % simexVersion,
+    "thediscprog" %% "util-library" % simexVersion,
     "com.github.fd4s" %% "fs2-kafka" % "3.2.0",
     "com.beachape" %% "enumeratum" % "1.7.2",
     "com.beachape" %% "enumeratum-circe" % "1.7.2",
     "io.circe" %% "circe-core" % "0.14.5",
     "io.circe" %% "circe-generic" % "0.14.5",
     "io.circe" %% "circe-parser" % "0.14.5",
-    "io.circe" %% "circe-refined" % "0.14.5",
     "io.circe" %% "circe-generic-extras" % "0.14.3",
     "org.typelevel" %% "cats-effect" % "3.4.8",
     "org.typelevel" %% "log4cats-slf4j" % "2.6.0",
     "org.scalatest" %% "scalatest" % "3.2.15" % "test",
-    "eu.timepit" %% "refined" % "0.11.0",
     "org.typelevel" %% "cats-effect-testing-scalatest" % "1.4.0" % Test,
     "ch.qos.logback" % "logback-classic" % "1.4.11" % Test,
     "com.github.blemale" %% "scaffeine" % "5.2.1" % Test
